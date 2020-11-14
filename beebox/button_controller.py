ee10ea74f4c4c6b6bc8a502233c0f4f8d9f36ede
Nicolai Schönberg <nicolai.schoenberg@gmail.com>
2020-11-14 17:41:52 +0100

    Implemented shutdown command

--- beebox/button_controller.py
+++ beebox/button_controller.py
@@ -1,5 +1,6 @@
 from gpiozero import Button
 from signal import pause
+from subprocess import check_call
 
 def say_hello():
     print("Hello!")
@@ -9,8 +10,9 @@ def say_goodbye():
 
 def shutdown():
     print("shutdown")
+    check_call(['sudo', 'poweroff'])
 
-button = Button(23, hold_time=5)
+button = Button(23, hold_time=3)
 
 button.when_pressed = say_hello
 button.when_released = say_goodbye
