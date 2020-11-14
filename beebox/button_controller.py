68ed576fca8c9fb738d10c59715c83674bf75fcc
Nicolai Schönberg <nicolai.schoenberg@gmail.com>
2020-11-14 17:59:20 +0100

    Added a second button used to wake pi when in halt mode. Has functions assigned for later use when not in halt mode

--- beebox/button_controller.py
+++ beebox/button_controller.py
@@ -2,20 +2,30 @@ from gpiozero import Button
 from signal import pause
 from subprocess import check_call
 
-def say_hello():
-    print("Hello!")
+def red_pressed():
+    print("red pressed")
 
-def say_goodbye():
-    print("Goodbye!")
+def red_released():
+    print("red released")
 
-def shutdown():
+def red_held():
     print("shutdown")
     check_call(['sudo', 'poweroff'])
 
-button = Button(23, hold_time=3)
+def green_pressed():
+    print("green pressed")
 
-button.when_pressed = say_hello
-button.when_released = say_goodbye
-button.when_held = shutdown
+def green_released():
+    print("green released")
+
+red_button = Button(23, hold_time=3)
+red_button.when_pressed = red_pressed
+red_button.when_released = red_released
+red_button.when_held = red_held
+
+green_button = Button(3)
+green_button.when_pressed = green_pressed
+green_button.when_released = green_released
+
 
 pause()
